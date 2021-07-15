830904a0f6416bdfb5e744ef3a04244c3f74dc86
Hritik Kumar <hrk10321@gmail.com>
2021-07-15 10:59:18 +0530

    Modified app.js

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin')
+const connectDB = require('./config/db')
+
+connectDB();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 })
+
+app.use(adminRoutes.routes);
 
 app.listen(process.env.PORT, () => {
     console.log('Listening at PORT',process.env.PORT);
