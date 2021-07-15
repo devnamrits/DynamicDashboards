36500bfba15c7410e33d4a8292ea114cc2f8ad4a
Hritik Kumar <hrk10321@gmail.com>
2021-07-15 09:10:07 +0530

    Checked the server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('Listening at PORT',process.env.PORT);
+})
+
