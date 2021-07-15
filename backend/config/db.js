2f73a9e3a301e5b9eaa811ca9230fb49500a821b
Hritik Kumar <hrk10321@gmail.com>
2021-07-15 10:50:08 +0530

    Configured MongoDB database

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const db = process.env.DBURI;
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(
+        db,
+        {
+          useNewUrlParser: true
+        }
+      );
+  
+      console.log('MongoDB is Connected...');
+    } catch (err) {
+      console.error(err.message);
+      process.exit(1);
+    }
+  };
+
+  module.exports = connectDB;
